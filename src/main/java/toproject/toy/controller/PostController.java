cf129f5bff824a14bd11603ae191d406247a5bcb
gleehave <gleehave@gmail.com>
2022-12-03 12:36:51 +0900

    [add] 상수 추가

--- src/main/java/toproject/toy/controller/PostController.java
+++ src/main/java/toproject/toy/controller/PostController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.*;
 import toproject.toy.dto.PostDto;
 import toproject.toy.dto.PostResponse;
 import toproject.toy.service.PostService;
+import toproject.toy.utils.AppConstants;
 
 import javax.websocket.server.PathParam;
 import java.util.List;
+
+import static toproject.toy.utils.AppConstants.*;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -27,10 +30,10 @@ public class PostController {
     }
 
     @GetMapping("/list")
-    public PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-                                    @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-                                    @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-                                    @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir){
+    public PostResponse getAllPosts(@RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+                                    @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize,
+                                    @RequestParam(value = "sortBy", defaultValue = DEFAULT_SORT_BY, required = false) String sortBy,
+                                    @RequestParam(value = "sortDir", defaultValue = DEFAULT_SORT_DIRECTION, required = false) String sortDir){
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
