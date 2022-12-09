cbcbaee18f9877fc89371c9a72528c3db993c0a2
gleehave <gleehave@gmail.com>
2022-12-09 14:05:58 +0900

    [add] test unauthroization

--- src/test/java/toproject/toy/controller/CommentControllerTest.java
+++
@@ -1,7 +0,0 @@
-package toproject.toy.controller;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CommentControllerTest {
-
-}
--- src/test/java/toproject/toy/controller/PostControllerTest.java
+++ src/test/java/toproject/toy/controller/PostControllerTest.java
@@ -2,53 +2,51 @@ package toproject.toy.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import toproject.toy.dto.PostDto;
-import toproject.toy.dto.PostResponse;
 import toproject.toy.service.PostService;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(PostController.class)
-class PostControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+public class PostControllerTest {
+
+    @Mock
+    private User user;
+
+    @Autowired
+    private PostService postService;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
-    private PostService postService;
-
     @Test
-    void createPost() throws Exception{
-        String title = "This is Title";
-        String desciption = "This is Description";
-        String content = "This is Content";
-
+    void createPost() throws Exception {
         PostDto postDto = new PostDto();
         postDto.setId(1L);
-        postDto.setTitle(title);
-        postDto.setDescription(desciption);
-        postDto.setContent(content);
+        postDto.setTitle("TITLE");
+        postDto.setContent("Content");
+        postDto.setDescription("This is desciprtion");
 
-        ResponseEntity<PostDto> response = new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
-
-        Mockito.when(postService.createPost(postDto)).thenReturn(postDto);
-
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/posts/create")
+                .content(objectMapper.writeValueAsString(postDto)))
+                .andExpect(status().isUnauthorized());
     }
 }
