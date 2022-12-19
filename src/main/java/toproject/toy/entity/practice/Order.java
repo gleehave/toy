c8be79d5f38ef1e6bf6effeeb4a858c462058b02
gleehave <gleehave@gmail.com>
2022-12-19 13:24:40 +0900

    [modify] 양방향

--- src/main/java/toproject/toy/entity/practice/Order.java
+++ src/main/java/toproject/toy/entity/practice/Order.java
@@ -5,7 +5,9 @@ import lombok.Setter;
 import toproject.toy.entity.practice.entityEnum.OrderStatus;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -19,9 +21,25 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<OrderItem>();
+
     @Temporal(value = TemporalType.DATE)
     private Date orderDate;
 
     @Enumerated(value = EnumType.STRING)
     private OrderStatus status;
+
+    public void setMember(Member member) {
+        if (this.member != null) {
+            this.member.getOrders().remove(this);
+        }
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
 }
--- src/main/java/toproject/toy/entity/practice/OrderItem.java
+++ src/main/java/toproject/toy/entity/practice/OrderItem.java
@@ -1,24 +1,28 @@
 package toproject.toy.entity.practice;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "orderitems")
+@Getter @Setter
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_item_id")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
 
-    @Column(name = "item_id", nullable = false)
-    private Long itemId;
-
-    @Column(name = "order_id", nullable = false)
-    private Long orderId;
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 
     private Integer orderPrice;
+
     private Integer count;
-
-
 }
